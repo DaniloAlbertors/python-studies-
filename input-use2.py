bc64e50a891742dced7e762dd8fa8b524211ced1
Danilo Alberto <danilossalbertoss@gmail.com>
2026-06-07 16:52:10 -0300

    Update input-use2.py

--- input-use2.py
+++ input-use2.py
@@ -1,6 +1,6 @@
 height = float(input("What is the height of the rectangle? "))
 width = float(input("What is the width of the rectangle? "))
-## Calculation of the area of ​​a rectangle
+# Calculation of the area of ​​a rectangle
 area = height * width
 
-print(f"The area of this rectangle is {area:.2f}") ##: .2f Formats a decimal number to display exactly 2 decimal places, rounding when necessary.
+print(f"The area of this rectangle is {area:.2f}") #: .2f Formats a decimal number to display exactly 2 decimal places, rounding when necessary.
